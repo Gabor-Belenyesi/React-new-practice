d6fc1f4e6f10134f34280ff427be2c4933ad3827
Gabor-Belenyesi <belenyesig@gmail.com>
2022-11-30 10:15:55 +0100

    button-change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,14 @@ import './App.css'
 /* import Hello from './components/Hello' */
 import beersData from "./Data"
 import Beers from './components/Beers'
+import Button from './components/Button'
+
+
 
 function App() {
   return (
     <div className="App">
+      <Button />
       <Beers data ={beersData.cards}/>  
     </div>
   );
